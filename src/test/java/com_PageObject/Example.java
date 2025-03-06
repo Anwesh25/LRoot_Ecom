688d04710db7cf26f1c8eb97f72cbd462c0c1225
Anwesh25 <anweshsriramoju7475@gmail.com>
2025-03-06 17:28:46 +0530

    add

---
+++ src/test/java/com_PageObject/Example.java
@@ -0,0 +1,18 @@
+package com_PageObject;
+
+public class Example {
+	
+	public void Add01() {
+		int v1 = 10;
+		int v2 = 20;
+		System.out.println(v1+v2);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Example ex = new Example();
+		ex.Add01();
+	}
+
+}
